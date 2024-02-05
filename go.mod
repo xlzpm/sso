6fb00a2944687b6aafbb7b55b1bd1f9fe33bda65
Ilya Stone <tnt228337@gmail.com>
2024-02-05 17:26:45 +0300

    add interceptor

--- go.mod
+++ go.mod
@@ -35,6 +35,7 @@ require (
 require (
 	github.com/BurntSushi/toml v1.3.2 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible
+	github.com/grpc-ecosystem/go-grpc-middleware/v2 v2.0.1
 	github.com/ilyakaznacheev/cleanenv v1.5.0 // direct
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
--- internal/app/grpc/app.go
+++ internal/app/grpc/app.go
@@ -1,12 +1,17 @@
 package grpcapp
 
 import (
+	"context"
 	"fmt"
 	"log/slog"
 	"net"
 
+	"github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/logging"
+	"github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/recovery"
 	authgrpc "github.com/xlzpm/sso/internal/grpc/auth"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type App struct {
@@ -16,7 +21,24 @@ type App struct {
 }
 
 func New(log *slog.Logger, authService authgrpc.Auth, port int) *App {
-	gRPCServer := grpc.NewServer()
+	loggingOpts := []logging.Option{
+		logging.WithLogOnEvents(
+			logging.PayloadReceived, logging.PayloadSent,
+		),
+	}
+
+	recoveryOpts := []recovery.Option{
+		recovery.WithRecoveryHandler(func(p interface{}) (err error) {
+			log.Error("Recovered from panic", slog.Any("panic", p))
+
+			return status.Errorf(codes.Internal, "internal error")
+		}),
+	}
+
+	gRPCServer := grpc.NewServer(grpc.ChainUnaryInterceptor(
+		recovery.UnaryServerInterceptor(recoveryOpts...),
+		logging.UnaryServerInterceptor(InterceptorLogger(log), loggingOpts...),
+	))
 
 	authgrpc.Register(gRPCServer, authService)
 
@@ -25,6 +47,13 @@ func New(log *slog.Logger, authService authgrpc.Auth, port int) *App {
 		gRPCServer: gRPCServer,
 		port:       port,
 	}
+}
+
+func InterceptorLogger(l *slog.Logger) logging.Logger {
+	// TODO implement password in logs
+	return logging.LoggerFunc(func(ctx context.Context, lvl logging.Level, msg string, fields ...any) {
+		l.Log(ctx, slog.Level(lvl), msg, fields...)
+	})
 }
 
 func (a *App) MustRun() {
