4d3abc767e248e90c7bcb17d6e945bbd9be1c67e
Ilya Stone <tnt228337@gmail.com>
2024-02-06 12:22:09 +0300

    Added test on check NoError in func NewToken

--- tests/jwt_token_test.go
+++ tests/jwt_token_test.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"github.com/xlzpm/sso/internal/domain/models"
 	myjwt "github.com/xlzpm/sso/internal/lib/jwt"
 )
@@ -20,10 +21,8 @@ func TestCreateNewToken(t *testing.T) {
 		Secret: "AYE",
 	},
 		time.Second)
-	if err != nil {
-		panic(err)
-	}
 	assert.NotEqual(t,
 		"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBfaWQiOjEsImVtYWlsIjoiY3VycyIsImV4cCI6MTcwNTU5MzEwOSwidWlkIjoxfQ.iKZkg1HTmbOoXJTC_ZKWHAVPb45wXsFCRWPJWY5orTA",
 		str)
+	require.NoError(t, err)
 }
